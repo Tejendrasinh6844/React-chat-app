5189bc8f4bba710f43fc9fcb6eb6ca860f32dc3e
Tejendrasinh6844 <149938061+Tejendrasinh6844@users.noreply.github.com>
2025-01-04 07:01:35 +0530

    comment

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -15,3 +15,4 @@ const List = () => {
 }
 
 export default List
+
--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -1,21 +1,21 @@
 import "./userInfo.css"
-
 const Userinfo = () => {
   return (
     <>
-    <div className='UserInfo'>
+      <div className='UserInfo'>
         <div className="user">
-          <img src="./avatar.png" alt="avatar image" />
+          <img src="./avatar.jpg" alt="avatar image" />
+
           <h2>Tejendrasinh</h2>
         </div>
-           
+
         <div className="icons">
-            <img src="./more.png" alt=""  />
-            <img src="./video.png" alt=""  />
-            <img src="./edit.png" alt=""  />
+          <img src="./more.png" alt=""  />
+          <img src="./video.png" alt="" />
+          <img src="./edit.png" alt="" />
 
         </div>
-    </div>
+      </div>
     </>
   )
 }
